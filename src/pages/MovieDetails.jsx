f48155f4e286578ccccb90c23e4708868bdad047
sara-prioriello <saraprioriello6@gmail.com>
2026-07-12 14:01:30 +0200

    add milestone4

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -7,7 +7,10 @@ function MovieDetails() {
     const { id } = useParams();
 
     const [movie, setMovie] = useState(null);
+    const [reviews, setReviews] = useState([]);
     const url = `http://localhost:3000/movies/${id}`;
+
+
 
     function fetchMovies(url) {
         fetch(url)
@@ -15,9 +18,17 @@ function MovieDetails() {
             .then(data => setMovie(data))
             .catch(error => console.error(error))
     }
+
+    function fetchReviews(id) {
+
+        fetch(`http://localhost:3000/reviews/movies/${id}`)
+            .then(response => response.json())
+            .then(data => setReviews(data))
+            .catch(error => console.error(error));
+    }
     useEffect(() => {
-        fetchMovies(url);
-    }, [url])
+        fetchMovies(url), fetchReviews(id);
+    }, [id])
 
     return (
 
@@ -32,12 +43,26 @@ function MovieDetails() {
                             className="img-fluid rounded-3"
                         />
                     </div>
+
+
                     <div className="col-12 col-md-8">
                         <h1>{movie ? movie.title : "Movie Title"}</h1>
                         <div className='text-sm'>Director: {movie ? movie.director : "John Doe"}</div>
                         <div className='text-sm'>Year: {movie && movie.release_year}</div>
                         <div className="pt-3"><strong>Abstract</strong>: {movie ? movie.abstract : "This is a brief description of the movie."}</div>
                     </div>
+                    <div className="container">
+                        <h3><strong>Reviews:</strong></h3>
+                        {reviews.map((review) => (
+
+                            <div className="col-12 col-md-8" key={review.id}>
+                                <div className="text-sm"><strong>{review.name}</strong></div>
+                                <div className="text-sm">{review.text}</div>
+                            </div>
+
+                        ))}
+                    </div>
+
                 </div>
             </div>
         </ section>
